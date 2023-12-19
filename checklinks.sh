31d928541d065a00e28d88cc767f22d8765d41ce
Joseph Britton <85815005+JoeeBritton@users.noreply.github.com>
2023-12-18 23:19:12 -0800

    Update script to add confirmation message on success

--- checklinks.sh
+++ checklinks.sh
@@ -1,37 +1,40 @@
 #!/usr/bin/env sh
 
+e=0
 err() {
+    e=1
     >&2 echo "Error: $1"
 }
 
 testlink() {
     # test cannot have trailing slash
-    local file="$(echo "$2" | sed 's/\/$//')"
+    file="$(echo "$2" | sed 's/\/$//')"
 
     if [ ! -f "$file" ] && [ ! -d "$file" ]; then
         err "$2 does not exist"
-        return
+        return 1
     fi
 
     if [ ! -L "$file" ]; then
         err "$2 is not a symlink"
-        return
+        return 1
     fi
     
     if [ "$(readlink -f "$2")" != "$1" ]; then
         err "$2 does not link to $1"
-        return
+        return 1
     fi
+    return 0
 }
 
-testlink "$(realpath scripts/switchproj)" "$HOME/.local/bin/switchproj"
-testlink "$(realpath scripts/scratchtmux)" "$HOME/.local/bin/scratchtmux"
-testlink "$(realpath tmux/tmux.conf)" "$HOME/.config/tmux/tmux.conf"
-testlink "$(realpath foot/foot.ini)" "$HOME/.config/foot/foot.ini"
-testlink "$(realpath nvim/)" "$HOME/.config/nvim/"
-testlink "$(realpath zsh/.zshrc)" "$HOME/.zshrc"
-testlink "$(realpath zsh/.zprofile)" "$HOME/.zprofile"
-testlink "$(realpath alacritty/alacritty.yml)" "$HOME/.config/alacritty/alacritty.yml"
+testlink "$(realpath scripts/switchproj)" "$HOME/.local/bin/switchproj" 
+testlink "$(realpath scripts/scratchtmux)" "$HOME/.local/bin/scratchtmux" 
+testlink "$(realpath tmux/tmux.conf)" "$HOME/.config/tmux/tmux.conf" 
+testlink "$(realpath foot/foot.ini)" "$HOME/.config/foot/foot.ini" 
+testlink "$(realpath nvim/)" "$HOME/.config/nvim/" 
+testlink "$(realpath zsh/.zshrc)" "$HOME/.zshrc" 
+testlink "$(realpath zsh/.zprofile)" "$HOME/.zprofile" 
+testlink "$(realpath alacritty/alacritty.yml)" "$HOME/.config/alacritty/alacritty.yml" 
 
 tbpath="$(realpath thunderbird/user.js)"
 for item in ~/.thunderbird/*; do
@@ -40,3 +43,7 @@ for item in ~/.thunderbird/*; do
     fi
     testlink "$tbpath" "$item/user.js"
 done
+
+if [ $e = 0 ]; then
+    echo "All link checks passed."
+fi
