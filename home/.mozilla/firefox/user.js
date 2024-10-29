8e9714ea3f766c515c8664f7cb797a7ab93a9846
Joseph Britton <85815005+JosBritton@users.noreply.github.com>
2024-10-29 05:19:25 -0700

    Keep context menu open when middle-clicking bookmarks in firefox

--- home/.mozilla/firefox/user.js
+++ home/.mozilla/firefox/user.js
@@ -14,3 +14,4 @@ user_pref("media.rdd-ffmpeg.enabled", true);
 user_pref("media.av1.enabled", true);
 user_pref("gfx.x11-egl.force-enabled", true);
 user_pref("widget.dmabuf.force-enabled", true);
+user_pref("browser.bookmarks.openInTabClosesMenu", false);
