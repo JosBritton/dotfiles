3fdfb6355eed1dc2152ac0557077a1a3492afb4b
Joseph Britton <85815005+JosBritton@users.noreply.github.com>
2024-11-25 05:03:32 -0800

    Disable blinking cursors in zsh vi-mode config

--- config/zsh/vi.zsh
+++ config/zsh/vi.zsh
@@ -1,19 +1,19 @@
 function zle-keymap-select () {
     case $KEYMAP in
-        vicmd) echo -ne "\e[1 q";;      # block
-        viins|main) echo -ne "\e[5 q";; # beam
+        vicmd) echo -ne "\e[2 q";;      # block
+        viins|main) echo -ne "\e[6 q";; # beam
     esac
 }
 zle -N zle-keymap-select
 
 zle-line-init() {
     zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
-    echo -ne "\e[5 q"
+    echo -ne "\e[6 q"
 }
 zle -N zle-line-init
 
 _cursor_preexec_hook() {
-	echo -ne "\e[5 q"
+	echo -ne "\e[6 q"
 }
 
 # vi mode
@@ -23,4 +23,4 @@ export KEYTIMEOUT=1
 [[ -z $preexec_functions ]] && preexec_functions=()
 preexec_functions=($preexec_functions _cursor_preexec_hook)
 
-echo -ne "\e[5 q" # Use beam shape cursor on startup.
+echo -ne "\e[6 q" # Use beam shape cursor on startup.
